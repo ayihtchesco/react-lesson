3c197a347ce2d2ef2b381c01004c8ec86a54c8f0
Ayih Tchesco <61115022+ayihtchesco@users.noreply.github.com>
2023-06-23 13:02:32 +0000

    Update App.jsx and List.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,22 @@ import React, { useState } from 'react'
 import data from './data'
 import List from './List'
 function App() {
-	return <h2>reminder project setup</h2>
+	// create a state variable here
+	const [people, setPeople] = useState(data)
+
+	// this should clear all records
+	function clearAllRecords() {
+		setPeople([])
+	}
+	return (
+		<main>
+			<section className="container">
+				<h3>{people.length} birthdays today</h3>
+				<List people={people} />
+				<button onClick={clearAllRecords}>Clear All</button>
+			</section>
+		</main>
+	)
 }
 
 export default App
